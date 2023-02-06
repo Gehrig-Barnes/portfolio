ab821f37258bb8a7d0e11e00f629003150bd0f83
gehrig <gehrigbarnes@gmail.com>
2023-02-06 10:34:09 -0600

    updated card to have rows.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "../App.css";
 import "../style/projects.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function ProjectCard({ name, description, video, image }) {
   return (
     <div className="card">
-      <h2>{name}</h2>
-      <img id="project-image"src={image} />
-      <p>{description}</p> 
-      <a href={video}>Video Demo</a>
+      <Container>
+        <Row>
+          <Col>
+            <h2>{name}</h2>
+            <img id="project-image" src={image} />
+          </Col>
+          <Col>
+            <p>{description}</p>
+            <a href={video}>Video Demo</a>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/projectsData.js
+++ src/projectsData.js
@@ -3,19 +3,19 @@ const projects = [
       name: "Get Away",
       description: "Air BnB Clone",
       video: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      image:"https://m.media-amazon.com/images/I/81mzApc3+BL.jpg",
+      image:"https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg",
     },
     {
         name: "Cloud Watch",
         description: "Weather App",
         video: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        image:"https://m.media-amazon.com/images/I/81mzApc3+BL.jpg",
+        image:"https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg",
       },
       {
         name: "Planters",
         description: "Social Media App for Plant lovers",
         video: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        image:"https://m.media-amazon.com/images/I/81mzApc3+BL.jpg",
+        image:"https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg",
       },
   ];
 

b4e8b0514d9668496da06222123e7ebd86ffd305
gehrig <gehrigbarnes@gmail.com>
2023-02-27 11:03:32 -0600

    hopefully this fixes images that were pushed into production.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -158,7 +158,7 @@ function AboutMe({ isDark }) {
       <button
         className={toggleHide}
         id={isDark ? "backBtnDark" : "backBtn"}
-        onClick={() => navigate("/")}
+        onClick={() => navigate("/portfolio")}
         disabled={currY > prev ? true : false}
       >
         Back Home
